71f840f69356ae0548e8abf141c189aab7e54d8c
Swapnil Iqbal <swapniliqbal@gmail.com>
2018-08-20 18:51:50 -0700

    initial commit: already made a large portion of the app on local machine. Will improve upon from now

---
+++ server.js
@@ -0,0 +1,39 @@
+// Express initializes app to be a function handler that you can supply to an HTTP server (as seen in line 2).
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var users = [];
+var connected = [];
+var connectedCounter = 0;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// when connected
+io.on('connection', function (socket) {
+    connectedCounter = connectedCounter + 1;
+    console.log("a user connected");
+    console.log("connected users: " + connectedCounter);
+
+    // when a user is connected, get the "chat message" emit from index.html (its the value of the input in the form) and spit the msg
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
+    // When disconnected
+    socket.on('disconnect', function () {
+        connectedCounter = connectedCounter - 1;
+        console.log("a user disconnected");
+        console.log("connected users: " + connectedCounter);
+    })
+});
+
+// where the server starts
+http.listen(3000, function () {
+    console.log('Server started');
+});
+
+// ---------- Lead tester: ---------- //
+// Nadir El Tahir
